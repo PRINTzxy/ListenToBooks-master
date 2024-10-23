254f5ecf7fb1dc1344605a1b60f03ac40b456a9e
阿巴阿巴阿巴 <zxy177467@163.com>
2024-10-23 10:02:51 +0800

    Initial commit

---
+++ config/confjg.ts
@@ -0,0 +1,6 @@
+// 基础请求路径
+export const BASE_URL = "http://localhost:8500"
+// 基础上传路径
+export const BASE_UPLOAD_URL = "http://localhost:8500"
+// webSocket基础路径
+export const WebSocket_BASE_URL = "ws://localhost:8500/api/websocket"
